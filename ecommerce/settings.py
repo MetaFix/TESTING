5485bda36e99cd813b5487c31d28ec44457530b6
MetaFix <theparallaxchannel@gmail.com>
2021-06-17 01:18:51 +0300

    herolu add

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,3 +93,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
